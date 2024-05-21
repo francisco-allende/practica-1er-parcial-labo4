c240cdbc39a32f0739a1d87983c1c798be6145fd
fallende <franciscoallende@lakaut.com.ar>
2024-05-21 12:59:08 -0300

    login

--- practica-primer-parcial/src/app/components/login/login.component.ts
+++ practica-primer-parcial/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,20 +12,37 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class LoginComponent {
 
-  loginForm = new FormGroup({
-    user: new FormControl("", Validators.required),
-    password: new FormControl("", Validators.required)
-  });
+  loginForm:FormGroup;
+  loginUser:any = {email:'', password:''};
 
+  constructor(public fb: FormBuilder, private router: Router , private auth: AuthService) 
+  {
+    this.loginForm = this.fb.group({
+        email: new FormControl("", [Validators.required, Validators.email]),
+        password: new FormControl("", Validators.required)
+      });
+  }
+  
   login() {
 
     if (this.loginForm.invalid) {
       return;
     }
-
-    const userDataString = JSON.stringify(this.loginForm.value);
+    this.loginUser.email = this.loginForm.get('email')?.value;
+    this.loginUser.password = this.loginForm.get('password')?.value
+    const userDataString = JSON.stringify(this.loginUser);
 
     localStorage.setItem("current-user", userDataString);
+    this.router.navigateByUrl('');
+  }
+
+  hardcode(rol:string){
+    if(rol == "admin"){
+      this.loginForm.get('email')?.setValue("admin@gmail.com");
+    }else{
+      this.loginForm.get('email')?.setValue("empleado@gmail.com");
+    }
+    this.loginForm.get('password')?.setValue("123456");
 
   }
 }
